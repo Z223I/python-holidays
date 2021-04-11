6247e6193ed1080d5375792306fb1900f33b15fd
z223i <BruceWilsonUS@gmail.com>
2021-04-11 09:22:14 -0600

    Working short days only

--- holidays/countries/market_identifier_code.py
+++ holidays/countries/market_identifier_code.py
@@ -59,70 +59,107 @@ class MarketIdentifierCode(HolidayBase):
 
     def __init__(self, **kwargs):
         self.country = "MIC"
+
+        # TODO How to check if key exists before pop.
         self.shortDay = kwargs.pop("shortDay")
+        self.shortDaysOnly = kwargs.pop("shortDaysOnly")
         HolidayBase.__init__(self, **kwargs)
 
     def _populate(self, year):
 
         if self.state in ["XNYS", "XNAS"]:
-            # https://www.nyse.com/markets/hours-calendars
+            if not self.shortDaysOnly:
+                # https://www.nyse.com/markets/hours-calendars
 
-            # 2020
-            # Out of a possible 366 days, 104 days are weekend days (Saturday and Sunday) when the stock exchanges are closed. 
-            # Eight of the nine holidays which close the exchanges fall on weekdays, with Independence Day being observed on 
-            # Friday, July 3. 
-            # 
-            # 
-            # 
-            # 
-            # There are two shortened trading sessions: on Friday, November 27 (the day after Thanksgiving Day), 
-            # and on Thursday, December 24 (Christmas Eve).
-            # https://en.wikipedia.org/wiki/Trading_day#2020
+                # 2020
+                # Out of a possible 366 days, 104 days are weekend days (Saturday and Sunday) when the stock exchanges are closed. 
+                # Eight of the nine holidays which close the exchanges fall on weekdays, with Independence Day being observed on 
+                # Friday, July 3. 
+                # 
+                # 
+                # 
+                # 
+                # There are two shortened trading sessions: on Friday, November 27 (the day after Thanksgiving Day), 
+                # and on Thursday, December 24 (Christmas Eve).
+                # https://en.wikipedia.org/wiki/Trading_day#2020
 
-            # New Year's Day
-            if year > 1870:
-                name = "New Year's Day"
-                self[date(year, JAN, 1)] = name
-                if self.observed and date(year, JAN, 1).weekday() == SUN:
-                    self[date(year, JAN, 1) + rd(days=+1)] = name + " (Observed)"
+                # New Year's Day
+                if year > 1870:
+                    name = "New Year's Day"
+                    self[date(year, JAN, 1)] = name
+                    if self.observed and date(year, JAN, 1).weekday() == SUN:
+                        self[date(year, JAN, 1) + rd(days=+1)] = name + " (Observed)"
 
-                
-                # XNYS (US NYSE) Saturday January 1, 2022 the market stays fully open on Friday December 31, 2021.
-                # If this is true for all years for which New Year's Day falls on a Saturday, comment this out.
-                """
-                elif self.observed and date(year, JAN, 1).weekday() == SAT:
-                    # Add Dec 31st from the previous year without triggering
-                    # the entire year to be added
-                    expand = self.expand
-                    self.expand = False
-                    self[date(year, JAN, 1) + rd(days=-1)] = name + " (Observed)"
-                    self.expand = expand
-                # The next year's observed New Year's Day can be in this year
-                # when it falls on a Friday (Jan 1st is a Saturday)
-                if self.observed and date(year, DEC, 31).weekday() == FRI:
-                    self[date(year, DEC, 31)] = name + " (Observed)"
-                """
+                    
+                    # XNYS (US NYSE) Saturday January 1, 2022 the market stays fully open on Friday December 31, 2021.
+                    # If this is true for all years for which New Year's Day falls on a Saturday, comment this out.
+                    """
+                    elif self.observed and date(year, JAN, 1).weekday() == SAT:
+                        # Add Dec 31st from the previous year without triggering
+                        # the entire year to be added
+                        expand = self.expand
+                        self.expand = False
+                        self[date(year, JAN, 1) + rd(days=-1)] = name + " (Observed)"
+                        self.expand = expand
+                    # The next year's observed New Year's Day can be in this year
+                    # when it falls on a Friday (Jan 1st is a Saturday)
+                    if self.observed and date(year, DEC, 31).weekday() == FRI:
+                        self[date(year, DEC, 31)] = name + " (Observed)"
+                    """
 
-            # Martin Luther King Jr. Day
-            if year >= 1986:
-                name = "Martin Luther King Jr. Day"
-                self[date(year, JAN, 1) + rd(weekday=MO(+3))] = name
+                # Martin Luther King Jr. Day
+                if year >= 1986:
+                    name = "Martin Luther King Jr. Day"
+                    self[date(year, JAN, 1) + rd(weekday=MO(+3))] = name
 
-            # Washington's Birthday
-            name = "Washington's Birthday"
-            if year > 1970:
-                self[date(year, FEB, 1) + rd(weekday=MO(+3))] = name
-            elif year >= 1879:
-                self[date(year, FEB, 22)] = name
+                # Washington's Birthday
+                name = "Washington's Birthday"
+                if year > 1970:
+                    self[date(year, FEB, 1) + rd(weekday=MO(+3))] = name
+                elif year >= 1879:
+                    self[date(year, FEB, 22)] = name
 
-            # Good Friday
-            self[easter(year) + rd(weekday=FR(-1))] = "Good Friday"
+                # Good Friday
+                self[easter(year) + rd(weekday=FR(-1))] = "Good Friday"
 
-            # Memorial Day
-            if year > 1970:
-                self[date(year, MAY, 31) + rd(weekday=MO(-1))] = "Memorial Day"
-            elif year >= 1888:
-                self[date(year, MAY, 30)] = "Memorial Day"
+                # Memorial Day
+                if year > 1970:
+                    self[date(year, MAY, 31) + rd(weekday=MO(-1))] = "Memorial Day"
+                elif year >= 1888:
+                    self[date(year, MAY, 30)] = "Memorial Day"
+
+                # Independence Day
+                if year > 1870:
+                    name = "Independence Day"
+                    self[date(year, JUL, 4)] = name
+                    if self.observed and date(year, JUL, 4).weekday() == SAT:
+                        self[date(year, JUL, 4) + rd(days=-1)] = name + " (Observed)"
+                    elif self.observed and date(year, JUL, 4).weekday() == SUN:
+                        self[date(year, JUL, 4) + rd(days=+1)] = name + " (Observed)"
+
+                # Labor Day
+                if year >= 1894:
+                    self[date(year, SEP, 1) + rd(weekday=MO)] = "Labor Day"
+
+                # Thanksgiving
+                if year > 1870:
+                    self[date(year, NOV, 1) + rd(weekday=TH(+4))] = "Thanksgiving"
+
+                # Christmas Day
+                if year > 1870:
+                    name = "Christmas Day"
+                    self[date(year, DEC, 25)] = "Christmas Day"
+                    if self.observed and date(year, DEC, 25).weekday() == SAT:
+                        self[date(year, DEC, 25) + rd(days=-1)] = name + " (Observed)"
+                    elif self.observed and date(year, DEC, 25).weekday() == SUN:
+                        self[date(year, DEC, 25) + rd(days=+1)] = name + " (Observed)"
+
+
+
+
+            #
+            # Allows do this section.
+            #
 
             # Independence Day Eve
             # Partial day on July 3 if July 4 lands on Tues. - Fri.  This is verified true for 2021.
@@ -137,29 +174,12 @@ class MarketIdentifierCode(HolidayBase):
                     (self.shortDay and date(year, JUL, 3).weekday() == THU)):
                     self[date(year, JUL, 3)] = name
 
-            # Independence Day
-            if year > 1870:
-                name = "Independence Day"
-                self[date(year, JUL, 4)] = name
-                if self.observed and date(year, JUL, 4).weekday() == SAT:
-                    self[date(year, JUL, 4) + rd(days=-1)] = name + " (Observed)"
-                elif self.observed and date(year, JUL, 4).weekday() == SUN:
-                    self[date(year, JUL, 4) + rd(days=+1)] = name + " (Observed)"
-
-            # Labor Day
-            if year >= 1894:
-                self[date(year, SEP, 1) + rd(weekday=MO)] = "Labor Day"
-
-            # Thanksgiving
-            if year > 1870:
-                self[date(year, NOV, 1) + rd(weekday=TH(+4))] = "Thanksgiving"
-
             # Partial day on Friday
             # Day After Thanksgiving
             name = "Day After Thanksgiving (Short Trading Day)"
             if self.shortDay:
-               dt = date(year, NOV, 1) + rd(weekday=TH(+4))
-               self[dt + rd(days=+1)] = name
+            dt = date(year, NOV, 1) + rd(weekday=TH(+4))
+            self[dt + rd(days=+1)] = name
 
 
             # TODO:
@@ -168,29 +188,11 @@ class MarketIdentifierCode(HolidayBase):
             # https://en.wikipedia.org/wiki/Trading_day#2020
 
 
-
-
-            
-
-            # Christmas Day
-            if year > 1870:
-                name = "Christmas Day"
-                self[date(year, DEC, 25)] = "Christmas Day"
-                if self.observed and date(year, DEC, 25).weekday() == SAT:
-                    self[date(year, DEC, 25) + rd(days=-1)] = name + " (Observed)"
-                elif self.observed and date(year, DEC, 25).weekday() == SUN:
-                    self[date(year, DEC, 25) + rd(days=+1)] = name + " (Observed)"
-
             """ Partial trading day?  Does not look like it.
             # New Year's Eve
             # For Tues. December 31, 2020, the XNYS and XNAS had full sessions.
             """
-
 """
-class US(UnitedStates):
-    pass
-
-
-class USA(UnitedStates):
+class MIC(MarketIdentifierCode):
     pass
 """
